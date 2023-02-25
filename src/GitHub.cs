48696f42a79bda8ed47f47cc2d90d743c6eced16
osawa-koki <github@osawa.dev>
2023-02-25 21:04:57 +0900

    小さな修正。

--- src/GitHub.cs
+++ src/GitHub.cs
@@ -23,8 +23,41 @@ public static partial class Program
     }
     catch (Exception ex)
     {
-      Console.WriteLine(ex.Message);
+      logger.Error(ex.Message);
       return new();
     }
   }
+
+  public static List<string> GetGitLabRepos()
+  {
+    try
+    {
+      List<string> repos = new();
+
+      var page = 1;
+      var per_page = 100;
+      while (true)
+      {
+        var url = $"https://gitlab.example.com/api/v4/projects?owned=true&simple=true&membership=true&page={page}&per_page={per_page}";
+        var headers = new Dictionary<string, string>
+            {
+                { "Private-Token", "YOUR_PRIVATE_ACCESS_TOKEN" }
+            };
+        var gitlab_repos = HttpModule.GetJsonArray(url, headers).Select(x => x["path_with_namespace"].ToString()).ToList();
+        repos.AddRange(gitlab_repos);
+        page++;
+        if (gitlab_repos.Count < per_page)
+        {
+          break;
+        }
+      }
+      return repos;
+    }
+    catch (Exception ex)
+    {
+      logger.Error(ex.Message);
+      return new();
+    }
+  }
+
 }
